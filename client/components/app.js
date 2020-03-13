4ee30d4779534791ed79178a5ebf070fa610f1c4
Thao Le <ttle09@gmail.com>
2020-03-12 22:02:59 -0400

    complete initial set up

---
+++ client/components/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App(props) {
+  return <h1>I am a component!</h1>
+}
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import App from './components/app';
+
+const appDom = document.querySelector('#app');
+ReactDOM.render(<App/>, appDom);
