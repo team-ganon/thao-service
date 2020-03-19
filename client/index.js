ca3de81c80b94c8e88b7dbabe974a3425d0fa48c
Thao Le <ttle09@gmail.com>
2020-03-18 20:40:28 -0400

    render neighborhoodComponents

--- client/index.js
+++ client/index.js
@@ -3,18 +3,35 @@ import ReactDOM from "react-dom";
 import App from './components/app.jsx';
 const axios = require('axios');
 
+import API from './config.js';
 const appDom = document.querySelector('#app');
+const config = API.GoogleAPI;
+let areaData;
+let hostData;
 
-axios.get('/zip', {
+
+axios.get('/area', {
   params: {
-    zip: '26301-6161'
+    zip: '15080'
   }
 })
-.then(res => res.data[0])
-.catch(function (error) {
-  console.log(error);
+.then(res => {
+  areaData = res.data[0];
 })
-.then(data => {
-  ReactDOM.render(<App data={data}/>, appDom);
+.catch(err => console.log(err))
+.then(() => {
+  axios.get('/host', {
+    params: {
+      zip: '15080'
+    }
+  })
+  .then(res => {
+    hostData = res.data[0];
+  })
+  .catch(err => console.log(err))
+  .then(() => {
+    ReactDOM.render(<App area={areaData} host={hostData} api={config}/>, appDom);
+  })
 })
+
 
