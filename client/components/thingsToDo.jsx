84b7c407a2fa0d1c842a7b5409858cb200b13e7d
Thao Le <ttle09@gmail.com>
2020-03-22 18:54:31 -0400

    refactor component to leave entries within container

--- client/components/thingsToDo.jsx
+++ client/components/thingsToDo.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ThingsEntry from './thingsEntry.jsx'
 
 class ThingsToDo extends React.Component {
   constructor(props) {
@@ -14,7 +13,12 @@ class ThingsToDo extends React.Component {
         <h2>Things to do near this home</h2>
         <div>
           {this.props.data.map(thing => (
-            <ThingsEntry data={thing}/>
+            <div>
+            <img src={thing.image}></img>
+            <div>{thing.thingsType}</div>
+            <div>{thing.description}</div>
+            <div>{thing.cost}</div>
+          </div>
           ))}
         </div>
       </div>
