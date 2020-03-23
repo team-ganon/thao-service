70f7c0ad307988b41a7abbc78a6d74f67d60fdde
Thao Le <ttle09@gmail.com>
2020-03-23 15:44:50 -0400

    add readmore feature

--- client/components/neighborhood.jsx
+++ client/components/neighborhood.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
+import css from './neighborhood.module.css';
+import styleable from 'react-styleable';
 
 class Neighborhood extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: '',
+      showGettingAround: false,
+      GettingAround:
+      <div onClick={this.handleClickGetAround.bind(this)}>
+        <a href=''>Read more about the neighborhood</a>
+      </div>,
+      showRules: false,
     }
   }
+
+  handleClickGetAround(e) {
+    e.preventDefault();
+    if (this.state.showGettingAround) {
+      this.setState ({
+        showGettingAround: false,
+        GettingAround:
+        <div>
+          <a href='' onClick={this.handleClickGetAround.bind(this)}>Read more about the neighborhood</a>
+        </div>
+      })
+    } else {
+        this.setState ({
+          showGettingAround: true,
+          GettingAround:
+          <div>
+            <h3>Getting around</h3>
+            {this.props.data.location.gettingAround}
+            <a href='' onClick={this.handleClickGetAround.bind(this)}>Hide</a>
+          </div>
+        })
+    }
+  }
+
   render() {
    return (
-     <div>
-       <h2>The neighborhood</h2>
-       <p>
-         {`${this.props.data.name}'s place is located in ${this.props.data.city}.`} <br/>
+     <div className={this.props.css.root}>
+       <h2 className={this.props.css.title}>The neighborhood</h2>
+         <div className={this.props.css.location}>{`${this.props.data.name}'s place is located in ${this.props.data.city} ${this.props.data.state} United States.`}
+         </div>
+         <div className={this.props.css.body}>
          {this.props.data.location.body}
-         <h3>Getting around</h3>
-         {this.props.data.location.gettingAround}
-       </p>
+         </div>
+         {this.state.GettingAround}
        <iframe
          src={`https://www.google.com/maps/embed/v1/place?key=${this.props.api}
          &q=${this.props.data.zip}`}>
@@ -36,4 +67,4 @@ class Neighborhood extends React.Component {
   }
 }
 
-export default Neighborhood;
+export default styleable(css)(Neighborhood);
