87dee69780bdea658d383d36aae621b8bceea4dc
Thao Le <ttle09@gmail.com>
2020-03-23 18:40:05 -0400

    feature and style complete

--- client/components/neighborhood.jsx
+++ client/components/neighborhood.jsx
@@ -8,10 +8,13 @@ class Neighborhood extends React.Component {
     this.state = {
       showGettingAround: false,
       GettingAround:
-      <div onClick={this.handleClickGetAround.bind(this)}>
-        <a href=''>Read more about the neighborhood</a>
+      <div className={this.props.css.readNeigh}>
+        <a className={this.props.css.click} href='' onClick={this.handleClickGetAround.bind(this)}>Read more about the neighborhood <span className={`fas fa-chevron-down ${this.props.css.icon}`}></span></a>
       </div>,
       showRules: false,
+      Rules: <div className={this.props.css.readNeigh}>
+      <a className={this.props.css.click} href='' onClick={this.handleClickRules.bind(this)}>Read all rules<span className={`fas fa-chevron-down ${this.props.css.icon}`}></span></a>
+    </div>
     }
   }
 
@@ -21,18 +24,48 @@ class Neighborhood extends React.Component {
       this.setState ({
         showGettingAround: false,
         GettingAround:
-        <div>
-          <a href='' onClick={this.handleClickGetAround.bind(this)}>Read more about the neighborhood</a>
+        <div className={this.props.css.readNeigh}>
+          <a className={this.props.css.click} href='' onClick={this.handleClickGetAround.bind(this)}>Read more about the neighborhood<span className={`fas fa-chevron-down ${this.props.css.icon}`}></span></a>
         </div>
       })
     } else {
         this.setState ({
           showGettingAround: true,
           GettingAround:
-          <div>
+          <div className={this.props.css.readNeigh}>
             <h3>Getting around</h3>
-            {this.props.data.location.gettingAround}
-            <a href='' onClick={this.handleClickGetAround.bind(this)}>Hide</a>
+            <p>
+            {this.props.data.location.gettingAround} <br/>
+            Property is conveniently located near the bus line with limited service.
+            </p>
+            <div>
+            <a className={this.props.css.click} href='' onClick={this.handleClickGetAround.bind(this)}>Hide<span className={`fas fa-chevron-up ${this.props.css.icon}`}></span></a>
+            </div>
+          </div>
+        })
+    }
+  }
+
+  handleClickRules(e) {
+    e.preventDefault();
+    if (this.state.showRules) {
+      this.setState ({
+        showRules: false,
+        Rules:
+        <div className={this.props.css.readNeigh}>
+          <a className={this.props.css.click} href='' onClick={this.handleClickRules.bind(this)}>Read all rules<span className={`fas fa-chevron-down ${this.props.css.icon}`}></span></a>
+        </div>
+      })
+    } else {
+        this.setState ({
+          showRules: true,
+          Rules:
+          <div className={this.props.css.readNeigh}>
+            <h3>Additional Rules</h3>
+            <p>{this.props.data.rules.body}</p>
+            <div>
+            <a className={this.props.css.click} href='' onClick={this.handleClickRules.bind(this)}>Hide rules<span className={`fas fa-chevron-up ${this.props.css.icon}`}></span></a>
+            </div>
           </div>
         })
     }
@@ -44,24 +77,42 @@ class Neighborhood extends React.Component {
        <h2 className={this.props.css.title}>The neighborhood</h2>
          <div className={this.props.css.location}>{`${this.props.data.name}'s place is located in ${this.props.data.city} ${this.props.data.state} United States.`}
          </div>
-         <div className={this.props.css.body}>
+         <p className={this.props.css.body}>
          {this.props.data.location.body}
-         </div>
+         </p>
          {this.state.GettingAround}
+         <div className={this.props.css.map}>
        <iframe
          src={`https://www.google.com/maps/embed/v1/place?key=${this.props.api}
          &q=${this.props.data.zip}`}>
        </iframe>
-       <div>Exact location information is provided after a booking is confirmed.</div>
-       <h2>Things to keep in mind</h2>
-       <div>
-         Check-in: {this.props.data.rules.checkin} <br/>
-         Checkout: {this.props.data.rules.checkout}
-       </div>
-       <div>
+       <p className={this.props.css.mapP}>
+       Exact location information is provided after a booking is confirmed.
+       </p>
+         </div>
+       <h2 className={this.props.css.title}>Things to keep in mind</h2>
+       <p className={this.props.css.checkTime}>
+         <strong>Check-in:</strong> {this.props.data.rules.checkin} <br/>
+         <strong>Checkout:</strong> {this.props.data.rules.checkout}
+       </p>
          <h3>House Rules</h3>
-         {this.props.data.rules.body}
-       </div>
+         <div className={this.props.css.rulesBody}>
+           <p>
+           <div className={this.props.css.rulesIconOne}>
+         <span class="fas fa-baby-carriage"></span><span> &nbsp;&nbsp; Not suitable for children and infants</span>
+           </div>
+           <div ClassName={this.props.css.rulesIcontwo}>
+         <span class="fas fa-paw"></span><span> &nbsp; No pets</span>
+           </div>
+           <div className={this.props.css.rulesIconOne}>
+         <span class="fas fa-glass-cheers"></span><span> &nbsp; No parties or events</span>
+           </div>
+           <div className={this.props.css.rulesIcontwo}>
+         <span class="fas fa-smoking-ban"></span><span> &nbsp; Smoking is not allowed</span>
+           </div>
+           </p>
+         </div>
+         {this.state.Rules}
      </div>
    )
   }
