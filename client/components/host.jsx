0999bdcbce49141eaf2a4ce14e708c642c5e89b2
Thao Le <ttle09@gmail.com>
2020-03-23 23:05:54 -0400

    complete hostComponent Style and Feature

--- client/components/host.jsx
+++ client/components/host.jsx
@@ -1,42 +1,78 @@
 import React from 'react';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
-import StarIcon from '@material-ui/icons/Star';
+import css from './host.module.css';
+import styleable from 'react-styleable';
 
 class Host extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: '',
+      Body: <p>{this.props.data.body.slice(0, 200)}... <a className={this.props.css.links}href='' onClick={this.handleClick.bind(this)}>Read more</a></p>
     }
   }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState ({
+      Body: <p>{this.props.data.body}</p>
+    })
+  }
+
   render() {
     const data = this.props.data;
+    const style = this.props.css;
     return (
-      <div>
-        <div>
+      <div className={style.root}>
+        <div className={style.profile}>
           <h2>Hosted by {data.name}</h2>
-          <p>
-            {data.city}, {data.state} · Joined in {data.monthJoined} {data.yearJoined} <br/>
-            <StarIcon/> {data.review} Reviews <VerifiedUserIcon/> {data.verified}
+          <p className={style.location}>
+            {data.city}, {data.state} · Joined in {data.monthJoined} {data.yearJoined}
           </p>
-          <img src={data.image}></img>
+          <p>
+            <span class="fas fa-star"></span> &nbsp; {data.review} Reviews &nbsp; <span class="fas fa-badge-check"></span> &nbsp; {data.verified}
+          </p>
+          <div className={style.picWrapper}>
+          <img className={style.profilePic} src={data.image} width={60} height={60} mode='fit'></img>
+          </div>
         </div>
+        <div className={style.superhost}>
+          <p>
+            <span className={style.bold}>{data.name} is a Superhost ·</span> Superhosts are experienced, highly rated hosts <br/> who are committed to providing great stays for guests.
+          <div className={style.medalIcon}>
+            <span className={style.medalIcon} class="fad fa-medal" style={{'--fa-primary-color': 'orange', '--fa-secondary-color': 'dimgray'}}></span>
+          </div>
+          </p>
+        </div>
+            <div className={style.Body}>
+              {this.state.Body}
+            </div>
         <p>
-          {data.name} is a Superhost · Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.
-        </p>
-        <p>
-          {data.body}
+          <div className={style.interactionGuest}>
           <h3> Interaction with guests</h3>
-          {data.interaction} <br/>
-          Languages: English <br/>
-          Response rate: 100% <br/>
-          Response time: within an hour <br/>
-          <button>Contact host</button> <br/>
-          <span>Always communicate through Airbnb </span> · To protect your payment, never transfer money or communicate outside of the Airbnb website or app. <a href=''>Learn more</a>
+          {data.interaction}
+          </div>
+          <div className={style.info}>
+            <div className={style.info}>
+              Languages: <strong>English</strong>
+            </div>
+            <div className={style.info}>
+              Response rate: <strong>100%</strong>
+            </div>
+            <div className={style.info}>
+              Response time: <strong>within an hour</strong>
+            </div>
+          </div>
+          <div className={style.buttonWrap}>
+            <div className={style.links}>
+            <a>Contact host</a>
+            </div>
+          </div>
+          <div className={style.disclaimer}>
+          Always communicate through Airbnb · To protect your payment, never transfer money or communicate outside of the Airbnb website or app. <a className={style.links} href=''>Learn more</a>
+          </div>
         </p>
      </div>
     )
   }
 }
 
-export default Host;
+export default styleable(css)(Host);
